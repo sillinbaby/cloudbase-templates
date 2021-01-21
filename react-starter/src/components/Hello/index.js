26bc2c1bb5555d1e3a66449eea2cdb60ab0d683f
sillinbaby <pengqian.whu@gmail.com>
2021-01-21 17:11:36 +0800

    Update index.js

--- react-starter/src/components/Hello/index.js
+++ react-starter/src/components/Hello/index.js
@@ -24,18 +24,6 @@ export default function Hello() {
     <div className="hello">
       <h1>欢迎使用云开发 CloudBase React App</h1>
       <div>
-        <div>
-          <h2>
-            通过
-            <a
-              href="https://cloud.tencent.com/document/product/876/46177"
-              target="_blank"
-            >
-              未登录 + 安全规则
-            </a>
-            的方式调用云开发
-          </h2>
-        </div>
 
         <h2>调用云函数</h2>
         <p>
